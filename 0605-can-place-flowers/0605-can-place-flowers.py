92e6063d730d7f39e217d64d6c4aa3ce07bfbea1
vrindavnair-ai <vrindavnair21@gmail.com>
2025-12-21 19:29:39 +0400

    Time: 7 ms (74.36%), Space: 17.7 MB (99.35%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -1,30 +1,32 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
         length = len(flowerbed)
+        #to handle edge case of array of length 1
         if (length == 1): 
             if(flowerbed[0]==0 and n!=0):
                  n = n-1
                  flowerbed[0] = 1
         else:
             for i in range(0,length):
+                #To check if n == 0 if n =0 ,  no need of iteratio. Return true
                 if(n!=0):
-                    if(i==0):
+                    if(i==0): #for first element check only the next element
                         if (flowerbed[i]==0 and flowerbed[i+1]==0):
                             n = n-1
                             flowerbed[i] = 1
-                    elif (i==(length-1)):
+                    elif (i==(length-1)): ##for last element check only the previous element
                         if (flowerbed[i]==0 and flowerbed[i-1]==0):
                             n = n-1
                             flowerbed[i] = 1
 
                     else:
-                 #checking if the previous and next flower bed is empty
-            #plant until we reach the target n (n==0)
+                    #for all other elements checking if the previous and next flower bed is empty
+                    #plant until we reach the target n (n==0)
                         if(flowerbed[i-1]==0 and flowerbed[i] ==0 and flowerbed[i+1]==0):
                             n = n-1
                 #if the condition is true. the plot is planted 
                             flowerbed[i] = 1
-                else:
+                else: # Return true if n =0 without iteration
                     return True
 
         if n == 0:
