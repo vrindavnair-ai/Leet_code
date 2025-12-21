5966ce5c198cc12f4053f322d8d2921a4b7f95af
vrindavnair-ai <vrindavnair21@gmail.com>
2025-12-21 19:11:05 +0400

    Time: 9 ms (45.98%), Space: 17.9 MB (95.48%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -7,23 +7,25 @@ class Solution:
                  flowerbed[0] = 1
         else:
             for i in range(0,length):
-                if(i==0):
-                    if (flowerbed[i]==0 and flowerbed[i+1]==0 and n!=0):
-                        n = n-1
-                        flowerbed[i] = 1
-                elif (i==(length-1)):
-                    if (flowerbed[i]==0 and flowerbed[i-1]==0 and n!=0):
-                        n = n-1
-                        flowerbed[i] = 1
+                if(n!=0):
+                    if(i==0):
+                        if (flowerbed[i]==0 and flowerbed[i+1]==0):
+                            n = n-1
+                            flowerbed[i] = 1
+                    elif (i==(length-1)):
+                        if (flowerbed[i]==0 and flowerbed[i-1]==0):
+                            n = n-1
+                            flowerbed[i] = 1
 
-                else:
+                    else:
                  #checking if the previous and next flower bed is empty
             #plant until we reach the target n (n==0)
-                    if(flowerbed[i-1]==0 and flowerbed[i] ==0 and flowerbed[i+1]==0 and n!=0):
-                        n = n-1
+                        if(flowerbed[i-1]==0 and flowerbed[i] ==0 and flowerbed[i+1]==0):
+                            n = n-1
                 #if the condition is true. the plot is planted 
-                        flowerbed[i] = 1
-
+                            flowerbed[i] = 1
+                else:
+                    return True
 
         if n == 0:
             return True
