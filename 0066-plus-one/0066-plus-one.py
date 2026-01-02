01bc0529eb863a0fa01b348dc2e2f455aed445d3
vrindavnair-ai <vrindavnair21@gmail.com>
2026-01-02 23:25:27 +0400

    Time: 0 ms (100%), Space: 17.3 MB (90.41%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.py
@@ -0,0 +1,19 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        #Legth of the list
+        n = len(digits)
+
+        #loop start at last index 
+        for i in range (n-1,-1,-1):
+            #if the digit is more than 9, adding 1 will bring carry
+            #if digit is less than 9 add 1 and return the digits
+            if digits[i]<9:
+                #Add 1 to the last element
+                digits[i] += 1
+                return digits
+            #If the digit is greater than 9 then the last digit will become 0
+            digits[i] = 0
+        return [1]+digits
+
+
+        
